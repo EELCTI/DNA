2c54eb78970371b3ca0eff5973644b34ebcf78e5
Mandy <amanda.britosilva01@gmail.com>
2026-06-19 13:52:42 -0300

    Trabalho

---
+++ Projeto/index.php
@@ -0,0 +1,136 @@
+<?php
+
+$ProdutosFontes = [
+    [
+        'imagem' => '/assets/img/fonte.jpeg',
+        'titulo' => 'Fonte',
+        'Marca_fonte' => 'DATEN',
+        'Potencia_fonte' => '200 W',
+        'Situacao_fonte' => 'Não Testado',
+    ],
+
+    [
+        'imagem' => '/assets/img/fonte02.jpeg',
+        'titulo' => 'Fonte',
+        'Marca_fonte' => 'FSP GROUP INC',
+        'Potencia_fonte' => '300 W',
+        'Situacao_fonte' => 'Não Testado',
+    ],
+
+];
+
+$ProdutosMemorias = [
+    [
+        'imagem' => '/assets/img/fonte.jpeg',
+        'titulo' => 'Fonte',
+        'Marca_fonte' => 'DATEN',
+        'Potencia_fonte' => '200 W',
+        'Situacao_fonte' => 'Não Testado',
+    ],
+
+    [
+
+        'imagem' => '/assets/img/fonte02.jpeg',
+        'titulo' => 'Fonte',
+        'Marca_fonte' => 'WISE',
+        'Potencia_fonte' => '220 W',
+        'Situacao_fonte' => 'Não Testado',
+
+    ],
+
+    [
+
+        'imagem' => '/assets/img/fonte03.jpeg',
+        'titulo' => 'Fonte',
+        'Marca_fonte' => 'KNUP',
+        'Potencia_fonte' => '200 W',
+        'Situacao_fonte' => 'Não Testado',
+
+    ],
+
+    [
+
+        'imagem' => '/assets/img/fonte04.jpeg',
+        'titulo' => 'Fonte',
+        'Marca_fonte' => 'FSP GROUP INC',
+        'Potencia_fonte' => '300 W',
+        'Situacao_fonte' => 'Não Testado',
+
+    ]
+]
+
+
+?>
+<!doctype html>
+<html lang='pt-br'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DNA Eletrônicos</title>
+
+    <link rel="stylesheet" href="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+
+    <link rel="icon" href="/assets/img/favicon.ico" type="img/x-icon">
+    <link rel="stylesheet" href="/assets/style.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.html">
+                <img src="/assets/img/favicon.ico" alt="Logo" width="30" height="24"
+                    class="d-inline-block align-text-top">
+                DNA Eletrônicos
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/pages/admin.html">Admin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <section class='banner'>
+        <h2>Lista de Produtos</h2>
+    </section>
+
+    <div class="container-produto">
+        <div class="produtos">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <?php foreach ($ProdutosFontes as $fontes): ?>
+                        <img class="imagemFonte" src="<?php echo $fontes['imagem'] ?>" alt="">
+                        <h5 class="card-title text-center"> <?php echo $fontes['titulo'] ?></h5>
+                        <p class="card-text"><?php echo $fontes['Marca_fonte'] ?></p>
+                        <p class="card-text"><?php echo $fontes['Potencia_fonte'] ?></p>
+                        <p class="card-text"><?php echo $fontes['Situacao_fonte'] ?></p>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
